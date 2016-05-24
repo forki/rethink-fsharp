b1493873ec03b5468b551eaea00ffe1fc0c12f26
Paul Thomas <paul@codasolutions.co>
2016-05-24 16:57:42 +0100

    add unit tests to verify that the ScramAttributes can be converted into the expected client first and final message formats

---
+++ src/RethinkFSharp/ScramAttributes.fs
@@ -0,0 +1,35 @@
+﻿namespace RethinkFSharp.Network
+
+/// All permissible attributes that form part of the SCRAM authentication process as
+/// defined in RFC 5802, https://tools.ietf.org/html/rfc5802#section-5.1
+type internal ScramAttributes = 
+    {
+        AuthorizationIdentity : string option
+        Username : string option
+        Nonce : string option
+        HeaderAndChannelBinding : string option
+        Salt : string option
+        HashIterationCount : int option
+        ClientProof : string option
+        ServerSignature : string option
+        ServerError : string option
+    }
+    static member Default =
+        {
+            AuthorizationIdentity = None
+            Username = None
+            Nonce = None
+            HeaderAndChannelBinding = None
+            Salt = None
+            HashIterationCount = None
+            ClientProof = None
+            ServerSignature = None
+            ServerError = None
+        }
+
+module internal Scram =
+    let internal clientFirstMessage scramAttributes =
+        ""
+
+    let internal clientFinalMessage scramAttributes =
+        ""
---
+++ test/RethinkFSharpTests/ScramAttributesTests.fs
@@ -0,0 +1,51 @@
+﻿module ScramAttributesTests
+
+open Xunit
+open FsUnit.Xunit
+open RethinkFSharp.Network
+
+// This type is based on a SCRAM exchange as defined within RFC 5802, 
+// https://tools.ietf.org/html/rfc5802#section-5, this is used strictly for
+// message construction and does not represent a complete exchange.
+let private scramAttributes = 
+    {
+        AuthorizationIdentity = None
+        Username = Some("user")
+        Nonce = Some("fyko+d2lbbFgONRv9qkxdawL")
+        HeaderAndChannelBinding = Some("biws")
+        Salt = Some("QSXCR+Q6sek8bf92")
+        HashIterationCount = Some(4096)
+        ClientProof = Some("v0X8v3Bz2T0CJGbJQyF0X+HI4Ts=")
+        ServerSignature = Some("rmF9pqV8S7suAoZWja4dJRkFsKQ=")
+        ServerError = None
+    }
+
+[<Fact>]
+let ``a client first message should always start with n,,``() =
+    let firstMessage = Scram.clientFirstMessage scramAttributes
+    
+    firstMessage.Substring(0, 3) |> should equal "n,,"
+
+[<Fact>]
+let ``a client first message should contain a username, followed by a nonce``() =
+    let firstMessage = Scram.clientFirstMessage scramAttributes
+
+    let username = scramAttributes.Username.Value
+    let nonce = scramAttributes.Nonce.Value
+
+    let expectedFirstMessage = sprintf "u=%s,r=%s" username nonce
+
+    firstMessage.Substring(0, 3) |> should equal expectedFirstMessage
+
+[<Fact>]
+let ``a client final message should contain in order, a channel binding, nonce and client proof``() =
+    let finalMessage = Scram.clientFinalMessage scramAttributes
+
+    let headerAndChannelBinding = scramAttributes.HeaderAndChannelBinding.Value
+    let nonce = scramAttributes.Nonce.Value
+    let clientProof = scramAttributes.ClientProof.Value
+
+    let expectedFinalMessage = 
+        sprintf "c=%s,r=%s,p=%s" headerAndChannelBinding nonce clientProof
+
+    finalMessage |> should equal expectedFinalMessage
